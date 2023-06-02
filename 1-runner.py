b59a57e693e3f259a9316af50b9b873c82028ebc
Yoon Park <yoongon.park@gmail.com>
2023-06-02 21:58:24 +0900

    first commit

---
+++ 1-runner.py
@@ -0,0 +1,43 @@
+import json
+import os
+
+
+def hello(a, b):
+    c = a + b
+    return c
+def lambda_handler(event, context):
+    with open("1-user-code-input.txt", 'r') as f:
+        lines = f.readlines()
+    code = ''.join(lines)
+
+    with open("1-suffix.txt", 'r') as f:
+        lines = f.readlines()
+    suffix = ''.join(lines)
+
+    code += suffix
+
+    with open("/tmp/output.py", "w") as f:
+        f.write(code)
+
+    cmd = 'python3 /tmp/output.py'
+    results_string = os.popen(cmd).read().replace("\'", "\"")
+    results = json.loads(results_string)
+
+    for result in results:
+        input = result['input']
+        solution = hello(input[0], input[1])
+        result['solution_output'] = solution
+    return {
+        'statusCode': 200,
+        'body': json.dumps(results)
+    }
+
+
+res = lambda_handler(None, None)
+print(res)
+
+
+
+
+
+
